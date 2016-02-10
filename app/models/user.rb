657fc8c5dd949fa3821f91e00442e184505ee928
Tássyo Santana <tassyo.santana@hotmail.com>
2016-02-09 22:39:19 -0200

    autenticação com Facebook concluida

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,8 @@ class User < ActiveRecord::Base
 		user.name = auth_hash['info']['name']
     	user.location = auth_hash['info']['location']
     	user.image_url = auth_hash['info']['image']
-    	user.url = auth_hash['info']['urls']['Twitter']
+    	#user.url = auth_hash['info']['urls']['Twitter']
+        user.url = auth_hash['info']['urls'][user.provider.capitalize]
     	user.save!
     	user
 	end
--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,5 +1,6 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
   provider :twitter, ENV['TWITTER_KEY'], ENV['TWITTER_SECRET']
-  provider :facebook, ENV['FACEBOOK_KEY'], ENV['FACEBOOK_SECRET']
+  provider :facebook, ENV['FACEBOOK_KEY'], ENV['FACEBOOK_SECRET'],
+  scope: 'public_profile', info_fields: 'id,name,link'
 
 end
